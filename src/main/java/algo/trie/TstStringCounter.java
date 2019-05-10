bdca4ffa2d4bc74e0a85586b67967ecec309074d
Cause Chung <cuzfrog@139.com>
2019-05-10 22:49:01 +0800

    Add memory measure for String counter

--- src/main/java/algo/trie/TstStringCounter.java
+++ src/main/java/algo/trie/TstStringCounter.java
@@ -1,4 +1,10 @@
 package algo.trie;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 final class TstStringCounter {
+    static List<String> top(Stream<String> src, int n, int limit) {
+        return null;
+    }
 }
--- src/test/java/algo/trie/LongListStringCounterTest.java
+++ src/test/java/algo/trie/LongListStringCounterTest.java
@@ -1,23 +1,51 @@
 package algo.trie;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LongListStringCounterTest {
 
+    private final Stream<String> src = Stream.of("A", "A", "A", "B", "B", "C", "D", "D", "D", "D", "E");
+    private final Stream<String> infiniteSrc = Stream.generate(() -> RandomStringUtils.randomAlphabetic(3));
+
+    @Before
+    public void tryToClearMem() throws Exception {
+        Thread.sleep(200);
+        System.gc();
+        Thread.sleep(200);
+    }
+
     @Test
-    public void top() {
-        Stream<String> src = Stream.of("A", "A", "A", "B", "B", "C", "D", "D", "D", "D", "E");
+    public void rWayTrie() {
         assertThat(RTrieStringCounter.top(src, 3, 100)).containsExactly("D", "A", "B");
     }
 
     @Test
-    public void longList() {
-        Stream<String> src = Stream.generate(() -> RandomStringUtils.randomAlphabetic(3));
-        RTrieStringCounter.top(src, 10, 500000).forEach(System.out::println);
+    public void tsTrie() {
+        assertThat(TstStringCounter.top(src, 3, 100)).containsExactly("D", "A", "B");
+    }
+
+    @Test
+    public void rWayTrieMemMeasure() {
+        RTrieStringCounter.top(infiniteSrc, 10, 500000).forEach(System.out::println);
+        System.out.println("256RTrie: " + memUsage() + "MB");
+    }
+
+    @Test
+    public void hashMapMemMeasure() {
+        Map<String, Integer> counter = new HashMap<>();
+        infiniteSrc.limit(500000).forEach(str -> counter.compute(str, (k, v) -> v == null ? 1 : v + 1));
+        System.out.println("HashMap: " + memUsage() + "MB");
+    }
+
+    private static long memUsage() {
+        return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) >> 20;
     }
 }
